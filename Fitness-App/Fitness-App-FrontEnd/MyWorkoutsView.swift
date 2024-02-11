51e72463fd8fd07aec2dc378f2b504da1d8f80c5
Andrew Tischhauser <andrewtisc@hotmail.com>
2024-02-10 21:38:35 -0600

    MyWorkoutsView successfully navigating to WorkoutSummaryView.

--- Fitness-App/Fitness-App-FrontEnd/MyWorkoutsView.swift
+++ Fitness-App/Fitness-App-FrontEnd/MyWorkoutsView.swift
@@ -9,20 +9,21 @@ import SwiftUI
 
 struct MyWorkoutsView: View {
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
                 // Header for "My Workouts" menu
                 Text("My Workouts")
                 
                 // Run through a list of workouts and make a button per workout (takes you to "Workout Summary" view
                 ForEach(0..<5) { number in
-                    NavigationLink(destination: WorkoutSummaryView()) {
-                        Button("Workout Name \(number)"){
-                        // STUB: navigates to "Workout Summary" view for that workout
-                        }
+                    /*NavigationLink{
+                        WorkoutSummaryView()
+                    } label: {
+                        Button("Workout Name \(number)"){}
                             .buttonStyle(.borderedProminent)
-                            .padding()
-                    }
+                    }*/
+                    NavigationLink("Workout \(number)", destination: WorkoutSummaryView())
+                        .buttonStyle(.borderedProminent)
                 }
             }
         }
