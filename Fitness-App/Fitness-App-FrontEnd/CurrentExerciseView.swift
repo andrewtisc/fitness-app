04420e415b07479b1750041d8917279c111a4acb
Andrew Tischhauser <andrewtisc@hotmail.com>
2024-03-01 23:42:17 -0600

    Created CurrentExerciseView.swift. Views include CurrentExerciseView, DisplaySetView, and EditSetView.

---
+++ Fitness-App/Fitness-App-FrontEnd/CurrentExerciseView.swift
@@ -0,0 +1,108 @@
+//
+//  CurrentExerciseView.swift
+//  Fitness-App-FrontEnd
+//
+//  Created by Andrew Tischhauser on 3/1/24.
+//
+
+import SwiftUI
+
+struct CurrentExerciseView: View {
+    var workout: Workout
+    //var exercise: Exercise
+    @State private var currentExerciseNum: Int = 1
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    if currentExerciseNum > 1 {
+                        currentExerciseNum -= 1
+                    }
+                }, label: {
+                    Image(systemName: "arrowshape.left.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                })
+                Image(systemName: "globe")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .scaledToFit()
+                    .padding()
+                Button(action: {
+                    if currentExerciseNum < workout.exercises.count {
+                        currentExerciseNum += 1
+                    }
+                }, label: {
+                    Image(systemName: "arrowshape.right.circle")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                })
+            }
+            Text(workout.exercises[currentExerciseNum - 1].exerciseName)
+                .padding()
+            ForEach(workout.exercises[currentExerciseNum - 1].sets, id: \.self) { set in
+                DisplaySetView(set: set)
+            }
+            EditSetView()
+        }
+    }
+}
+struct DisplaySetView: View {
+    var set: Set
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "square.and.pencil")
+                .padding(.horizontal)
+            Text("Set \(set.id)")
+            Spacer()
+            Text("\(set.reps) reps @ \(set.weight) lbs")
+                .padding(.horizontal)
+        }
+    }
+}
+struct EditSetView: View {
+    //var exercise: Exercise
+    @State private var weight: Int = 45
+    @State private var reps: Int = 10
+    
+    var body: some View {
+        VStack {
+            Text("Set 4")
+            HStack {
+                VStack {
+                    HStack {
+                        Text("Weight: ")
+                            .padding(.horizontal)
+                        Stepper(value: $weight, in: 0...310, step: 5) {
+                            TextField("", value: $weight, formatter: NumberFormatter())
+                                .border(Color.black)
+                        }
+                    }
+                    HStack {
+                        Text("Reps: ")
+                            .padding(.horizontal)
+                        Stepper(value: $reps, in: 0...100, step: 1) {
+                            TextField("", value: $reps, formatter: NumberFormatter())
+                                .border(Color.black)
+                        }
+                    }
+                }
+                Button(action: {
+                    // STUB: Add set to exercise.sets somehow, with entered weight and reps
+                    //let se = Set(id: 99, reps: 999, weight: 9999)
+                    //let ex = Exercise(exerciseID: 1, exerciseName: "MadeUpExercise", sets: [se])
+                    //writeLocalJSON(codableStruct: ex, fileName: "testname")
+                }, label: {
+                    Text("Finish Set")
+                })
+                .buttonStyle(.borderedProminent)
+            }
+        }
+    }
+}
+
+#Preview {
+    CurrentExerciseView(workout: getLocalWorkout(workoutID: 1)!)
+}
