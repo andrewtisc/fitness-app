16796bb97f630d2976756aafc2e6b1f666fe451a
Andrew Tischhauser <andrewtisc@hotmail.com>
2024-03-24 00:16:45 -0500

    Added windows path option to get_workout api

--- Fitness-App/Fitness-App-BackEnd/workout_backend.py
+++ Fitness-App/Fitness-App-BackEnd/workout_backend.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 @app.route('/api/workouts/<int:workout_id>', methods=['GET'])
 def get_workout(workout_id):
     # Generate the path to the JSON file based on the book ID
-    json_file_path = f'/Users/andrewtischhauser/Developer/Projects/fitness-app-vscode/fitness-app/Fitness-App/Fitness-App-FrontEnd/Sample Data/workout_{workout_id}.json'
+    # json_file_path = f'/Users/andrewtischhauser/Developer/Projects/fitness-app-vscode/fitness-app/Fitness-App/Fitness-App-FrontEnd/Sample Data/workout_{workout_id}.json'   # macOS path
+    json_file_path = f'C:/Users/andre/code/fitness-app/Fitness-App/Fitness-App-FrontEnd/Sample Data/workout_{workout_id}.json'    # Windows path
     
     # Try to send the JSON file
     try:
         return send_file(json_file_path, mimetype='application/json')
     except FileNotFoundError:
-        return jsonify({'error': 'Book not found'}), 404
+        return jsonify({'error': 'Workout not found'}), 404
 
 if __name__ == '__main__':
     # Start ngrok tunnel
