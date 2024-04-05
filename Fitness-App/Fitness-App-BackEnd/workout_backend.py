30645da6c13a0bbf4d3e2219ca0b55d82b13d4e1
Andrew Tischhauser <andrewtisc@hotmail.com>
2024-04-05 03:44:16 -0500

    Edited comments to reflect changes

--- Fitness-App/Fitness-App-BackEnd/workout_backend.py
+++ Fitness-App/Fitness-App-BackEnd/workout_backend.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 # Route with parameter
 @app.route('/api/workouts/<int:workout_id>', methods=['GET'])
 def get_workout(workout_id):
-    # Generate the path to the JSON file based on the book ID
+    # Generate the path to the JSON file based on the workout ID
     json_file_path = f'/Users/andrewtischhauser/Developer/Projects/fitness-app-vscode/fitness-app/Fitness-App/Fitness-App-FrontEnd/Sample Data/workout_{workout_id}.json'
     
     # Try to send the JSON file
     try:
         return send_file(json_file_path, mimetype='application/json')
     except FileNotFoundError:
-        return jsonify({'error': 'Book not found'}), 404
+        return jsonify({'error': 'Workout not found'}), 404
 
 if __name__ == '__main__':
     # Start ngrok tunnel
